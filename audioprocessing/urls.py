4163d84fb2fba6a3c7f4ed45560a917ce286d83d
Aman Bishnoi <amanb1145@gmail.com>
2024-05-06 15:42:06 +0530

    Update urls.py

--- audioprocessing/urls.py
+++ audioprocessing/urls.py
@@ -1,8 +1,9 @@
 # audioprocessing/urls.py
 from django.urls import path
-from .views import ProcessAudioView, TaskStatusView
+from .views import ProcessAudioView, TaskStatusView, ExtractAudioView
 
 urlpatterns = [
     path('process-audio/', ProcessAudioView.as_view(), name='process_audio'),
     path('task-status/<str:task_id>/', TaskStatusView.as_view(), name='task_status'),
+    path('extract-audio/', ExtractAudioView.as_view(), name='extract_audio'),
 ]
